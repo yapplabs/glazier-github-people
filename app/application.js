98e57d02b87031c0870a854e88a6151cd939fe99
Luke Melia <luke@lukemelia.com>
2013-06-27 23:59:36 -0400

    WIP ember quit test

--- app/application.js
+++ app/application.js
@@ -1,9 +1,11 @@
 import 'resolver' as Resolver;
+import 'app/router' as Router;
 
 var App = Ember.Application.create({
   modulePrefix: 'app',
   rootElement: '#card',
-  resolver: Resolver
+  resolver: Resolver,
+  Router: Router
 });
 
 App.deferReadiness();
--- test/app/application_test.js
+++ test/app/application_test.js
@@ -1,8 +1,23 @@
+Ember.testing = true;
+import 'app/application' as App;
+import 'card' as card;
+debugger;
+
 module('github-people Application', {
-  setup: function(){},
-  teardown: function(){}
+  setup: function() {
+    debugger;
+    $('#qunit-fixture').append('<div id="card"></div>');
+    Ember.run(App, App.advanceReadiness);
+    App.injectTestHelpers();
+  },
+  teardown: function() {
+    App.reset();
+  }
 });
 
 test("first test", function(){
-  ok(true, "woot!")
+  expect(1);
+  visit("/").then(function() {
+    ok(true, "Woot!");
+  });
 });
