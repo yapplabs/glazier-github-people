b29679f4377d3448357ba8b4e09ed2a94ac83629
Luke Melia <luke@lukemelia.com>
2013-07-09 12:28:33 -0400

    Extract the ember_card_bridge

--- app/application.js
+++ app/application.js
@@ -8,40 +8,6 @@ var Application = Ember.Application.extend({
   Router: Router
 });
 
-var CardDataStore = Ember.Object.extend({
-  dataDidChange: function(bucket, data) {
-    var cardData;
-
-    if (bucket === '*') {
-      cardData = data;
-    } else {
-      cardData = {};
-      cardData[bucket] = data;
-    }
-    this.setProperties(cardData);
-  }
-});
-
-Application.initializer({
-  name: 'cardDataStore',
-  initialize: function(container, application) {
-    application.register('store:cardData', CardDataStore);
-    application.inject('controller', 'cardDataStore', 'store:cardData');
-    application.inject('route', 'cardDataStore', 'store:cardData');
-
-    var card = container.lookup('card:main'),
-        cardDataStore = container.lookup('store:cardData');
-    // TODO: Could we hook into dataConsumer to do this instead of defining a method?
-    if (card.didUpdateData) {
-      throw new Error('card already defines didUpdateData');
-    }
-    card.didUpdateData = function(bucket, data) {
-      cardDataStore.dataDidChange(bucket, data);
-    };
-    cardDataStore.dataDidChange('*', card.data);
-  }
-});
-
 requireModule('templates');
 
 export default Application;
