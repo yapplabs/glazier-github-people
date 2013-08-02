b2622ce2dd9a56963cba2aa598653ec8b54733a8
machty <machty@gmail.com>
2013-08-02 19:20:04 -0400

    Toolbar items: specify an array as a publishedProperty
    This is not beautiful (yet), but works as a way to propagate array changes
    to an array property specified in the `publishedProperties` array.
    
    -Steflex Matchpen

--- app/controllers/card_metadata.js
+++ app/controllers/card_metadata.js
@@ -4,9 +4,29 @@ var CardMetadataController = Ember.Controller.extend(RemoteEmberObjectMixin, {
   cardDataStore: null, //injected
   publishedProperties: [
     'isEditable',
-    'isEditing'
+    'isEditing',
+    'toolbar'
   ],
-  isEditable: Ember.computed.bool('cardDataStore.isAdmin')
+  isEditable: Ember.computed.bool('cardDataStore.isAdmin'),
+
+  // TODO: megahacks
+  toolbar: Ember.computed.alias('_toolbar'),
+  _toolbar: Ember.A([]),
+
+  toolbarDidChange: function(){
+    Ember.run.once(this, 'notifyToolbarChange');
+  }.observes('_toolbar.@each.text', '_toolbar.@each.icon'),
+
+  notifyToolbarChange: function(){
+    this.notifyPropertyChange('toolbar');
+  }
 });
+
+// ToolBarItem = Ember.Object.extend({
+//   text: function(){
+//     var length = this.get('length');
+//     return "" + length + " people";
+//   }.property('length')
+// });
 
 export default CardMetadataController;
